f643812b9957626400729439896cca076b33c825
joseivandelossantosgarcia <sagi040108hchnrva2@gmail.com>
2021-11-05 14:49:18 -0600

    Add files via upload

---
+++ articulo.php
@@ -0,0 +1,68 @@
+<?php include('header.php'); ?>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>ARTICULOS
+                    <br>REGISTRA YA TUS ARTICULOS DE COMPRA!!!</h1></br>
+			</div>
+         <head>
+         <div class="articulo">
+				<article>
+			
+            <form action="emplead.php" method="GET" name="farm">
+Nombre:    <input type="text" name= "Nombre"><br>
+
+Apellido:  <input type="text" name= "Apellido"><br>
+
+numero:<input type="text" name= "Numero"><br>
+
+Correo:    <input type="text" name= "Correo"><br>
+
+Enviar:    <input type="submit" value= "Enviar">
+            <html>
+   
+   <head>
+      
+               <p>LLENE EL FORMULARIO PARA VERIFICAR SUS ARTICULOS DE COMPRA</p>
+               <p>ADEMAS TENDRA ACCESO A SUS ARTICULOS SELECCIONADOS</p>
+
+               <br>1- llena el primer campo con tu nombre</br>
+               <br>2- llena el segundo campo con tu apellido</br>
+               <br>3- llena el tercer campo con el numero del articulo-id</br>
+               <br>4- llena el cuarto campo con tu correo personal</br>
+               <br>5- Ahora presiona enviar, para concluir el formulario, tus datos seran enviados.</br>
+</div>
+               <aside>
+				<div class="widget">
+					<h3>LOS ARTICULOS MAS RELEVANTES.</h3>
+					<ul>
+						<li><a href= "#">FRUTAS Y VERDURAS</a></li>
+						<li><a href= "#">CEREALES Y CARNES</a></li>
+						<li><a href= "#">LIQUIDOS Y FRITURAS</a></li>
+						<li><a href= "#">PAN Y GAELLETAS</a></li>
+                        <li><a href= "#">LECHE Y HUEVOS</a></li>
+                        <li><a href= "#">PRECIOS Y DESCUENTOS</a></li>
+
+                        </ul>
+                        <div cente><a href "#"><img src=img/articulos.jpg width="200" height="120" ></a></div>
+				
+                </div>
+
+                        
+</ul>
+				
+</div>
+			</aside>
+
+           
+
+      
+      
+
+</body>
+				
+</div>
+	</section>
+	<?php include('footer.php'); ?>
+</body>
+<?
---
+++ empleado.php
@@ -0,0 +1,70 @@
+<?php include('header.php'); ?>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>EMPLEADO
+                    <br> REGISTRATE YA PARA ACCEDER A TU CUENTA OFICIAL DE S-MART</br>
+                </h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+			
+            <form action="emplead.php" method="GET" name="farm">
+Nombre:    <input type="text" name= "Nombre"><br>
+
+Apellido:  <input type="text" name= "Apellido"><br>
+
+numero:<input type="text" name= "Numero"><br>
+
+Correo:    <input type="text" name= "Correo"><br>
+
+Enviar:    <input type="submit" value= "Enviar">
+            <html>
+               <p> ACCEDERAS A TUS DOCUMENTOS DE TRABAJO EN SMART COMO EMPLEADO</P>
+   <p> SOLO NECESITAS LLENAR EL FORMULARIO Y ENVIARLO</P>
+   
+      <head>
+      <p>LLENE EL FORMULARIO PARA VERIFICAR SUS ARTICULOS DE COMPRA</p>
+               <p>ADEMAS TENDRA ACCESO A SUS ARTICULOS SELECCIONADOS</p>
+
+               <br>1- llena el primer campo con tu nombre</br>
+               <br>2- llena el segundo campo con tu apellido</br>
+               <br>3- llena el tercer campo con TU numero de empleado de trabajo-id</br>
+               <br>4- llena el cuarto campo con tu correo personal</br>
+               <br>5- Ahora presiona enviar, para concluir el formulario, tus datos seran enviados.</br>
+</div>
+               <aside>
+				<div class="widget">
+					<h3>ACCEDE A TU INFORMACION DE TRABAJO...</h3>
+					<ul>
+						<li><a href= "#">SALDO DE COBRO</a></li>
+						<li><a href= "#">HORARIOS Y TURNO</a></li>
+						<li><a href= "#">SUCURSAL</a></li>
+						<li><a href= "#">ID DE EMPLEADO</a></li>
+                        <li><a href= "#">CANTIDAD DE PAGOS</a></li>
+                        <li><a href= "#">ADMINISTRACION</a></li>
+
+                        </ul>
+                        <div cente><a href "#"><img src=img/empleados.jpg width="220" height="180" ></a></div>
+				
+                </div>
+
+                        
+</ul>
+				
+</div>
+			</aside>
+
+           
+
+      
+      
+
+</body>
+				
+</div>
+	</section>
+	<?php include('footer.php'); ?>
+</body>
+<?
